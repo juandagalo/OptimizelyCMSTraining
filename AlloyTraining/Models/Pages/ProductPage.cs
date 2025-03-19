d71c18976f07938e6b689f307178800b2892c2aa
Juan Daniel Garcia Lopez <juandagarcial@gmail.com>
2025-03-19 14:56:20 -0500

    Exercise C4 done

--- AlloyTraining/Models/Pages/ProductPage.cs
+++ AlloyTraining/Models/Pages/ProductPage.cs
@@ -32,5 +32,19 @@ namespace AlloyTraining.Models.Pages
             Order = 320)]
         [Required]
         public virtual IList<string> UniqueSellingPoints { get; set; }
+
+        [Display(
+            Name = "Main content area",
+            Description = "Drag and drop blocks and pages with partial templates.",
+            GroupName = SystemTabNames.Content,
+            Order = 330)]
+        public virtual ContentArea MainContentArea { get; set; }
+
+        [Display(
+            Name = "Related content area",
+            Description = "Drag and drop blocks and pages with partial templates.",
+            GroupName = SystemTabNames.Content,
+            Order = 340)]
+        public virtual ContentArea RelatedContentArea { get; set; }
     }
 }

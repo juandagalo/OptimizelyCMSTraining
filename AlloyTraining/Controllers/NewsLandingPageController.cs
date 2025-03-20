96ca9a2943f952537c99bfd4dc166f1357eb91dd
Juan Daniel Garcia Lopez <juandagarcial@gmail.com>
2025-03-20 11:11:20 -0500

    Exercise E2 done

---
+++ AlloyTraining/Controllers/NewsLandingPageController.cs
@@ -0,0 +1,13 @@
+﻿using AlloyTraining.Models.Pages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    public class NewsLandingPageController(IContentLoader loader) : PageControllerBase<NewsLandingPage>(loader)
+    {
+        public IActionResult Index(NewsLandingPage currentPage)
+        {
+            return View(CreatePageViewModel(currentPage));
+        }
+    }
+}
---
+++ AlloyTraining/Models/Pages/NewsLandingPage.cs
@@ -0,0 +1,15 @@
+﻿using AlloyTraining.Models.Blocks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "News Landing", GUID = "{F84F9B7E-C417-48BE-9C3F-F5903EAB97B1}",
+        Description = "Use this as a landing page for a list of news articles.",
+        GroupName = SiteGroupNames.News)]
+    [SitePageIcon]
+    public class NewsLandingPage : StandardPage
+    {
+        [Display(Name = "News list", Order = 315)]
+        public virtual ListingBlock NewsListing { get; set; }
+    }
+}

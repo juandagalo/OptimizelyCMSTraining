6c109faca7c1cd9ec3092db860105d662c04bb21
Juan Daniel Garcia Lopez <juandagarcial@gmail.com>
2025-03-20 10:52:04 -0500

    Exercise E1 done

---
+++ AlloyTraining/Components/ListingBlockComponent.cs
@@ -0,0 +1,36 @@
+﻿using AlloyTraining.Models.Blocks;
+using AlloyTraining.Models.ViewModels;
+using EPiServer.Filters;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlloyTraining.Components
+{
+    public class ListingBlockComponent(IContentLoader loader) : BlockComponent<ListingBlock>
+    {
+        private readonly IContentLoader loader = loader;
+
+        protected override IViewComponentResult InvokeComponent(ListingBlock currentBlock)
+        {
+            ListingBlockViewModel viewmodel = new()
+            {
+                Heading = currentBlock.Heading
+            };
+
+            if (currentBlock.ShowChildrenOfThisPage != null)
+            {
+                IEnumerable<PageData> children = loader.GetChildren<PageData>(currentBlock.ShowChildrenOfThisPage);
+
+                // Remove pages:
+                // 1. that are not published
+                // 2. that the visitor does not have Read access to
+                // 3. that do not have a page template
+                IEnumerable<IContent> filteredChildren = FilterForVisitor.Filter(children);
+
+                // 4. that do not have "Display in navigation" selected
+                viewmodel.Pages = filteredChildren.Cast<PageData>().Where(page => page.VisibleInMenu);
+            }
+            return View(viewmodel);
+        }
+    }
+}
